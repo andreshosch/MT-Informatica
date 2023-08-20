4e6b65f0865481ec11fca3b421d177a78b461bf8
Andrés Hosch <andreshosch@hotmail.com>
2023-08-20 11:04:55 -0300

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
-import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
