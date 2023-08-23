85d633fe7a4621117f5d9b366df9fb52b0b7d879
Cristian Alessandria <cristian_alessandria@hotmail.com>
2023-08-22 23:30:10 -0300

    Fix - imagenes

--- src/app/components/gestion-productos/gestion-productos.component.ts
+++ src/app/components/gestion-productos/gestion-productos.component.ts
@@ -9,29 +9,6 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class GestionProductosComponent {
 
-  productosHot: any[] = [
-    {
-      nombre: "Auricular Axion Strem",
-      precio: "95",
-      rubro: "audio",
-      subrubro: "auriculares",
-      img: "https://elit.com.ar/producto/16153-aio-lenovo-think-centre-neo-30a-intel-core-i5-1240p-23-8-1x8gb-256gb-free-dos",
-      descripcion: "son una garcha"
-    },
-    {
-      nombre: "Camara niños",
-      precio: "256",
-      rubro: "video",
-      subrubro: "camara",
-      img: "16153-aio-lenovo-think-centre-neo-30a-intel-core-i5-1240p-23-8-1x8gb-256gb-free-dos",
-      descripcion: "safan dentro de todo"
-    }
-  ]
 
-  constructor(private _config: NgbCarouselConfig){
-    _config.interval = 2000;
-    _config.pauseOnHover = true;
-    _config.showNavigationArrows = false;
-  }
 
 }
--- src/app/components/ofertas-calientes/ofertas-calientes.component.ts
+++ src/app/components/ofertas-calientes/ofertas-calientes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-ofertas-calientes',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./ofertas-calientes.component.css']
 })
 export class OfertasCalientesComponent {
+
+  productosHot: any[] = [
+    {
+      nombre: "Auricular Axion Strem",
+      precio: "95",
+      rubro: "audio",
+      subrubro: "auriculares",
+      img: "https://http2.mlstatic.com/D_NQ_NP_2X_930027-MLA71145394601_082023-F.webp",
+      descripcion: "son una garcha"
+    },
+    {
+      nombre: "Camara niños",
+      precio: "256",
+      rubro: "video",
+      subrubro: "camara",
+      img: "https://http2.mlstatic.com/D_NQ_NP_2X_645359-MLA46557837279_062021-F.webp",
+      descripcion: "safan dentro de todo"
+    }
+  ]
+
+  constructor(private _config: NgbCarouselConfig){
+    _config.interval = 2000;
+    _config.pauseOnHover = true;
+    _config.showNavigationArrows = false;
+  }
 
 }
