f31a6ca16f71292d4a8d686045458bf7ca520f70
Cristian Alessandria <cristian_alessandria@hotmail.com>
2023-09-10 22:46:22 -0300

    Fix errores de Merge

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class ProductosService {
 
   private baseUrl: string
 
-  constructor(private http:HttpClient) { 
+  constructor(private http:HttpClient, private firestore: AngularFirestore) { 
     this.baseUrl="https://clientes.elit.com.ar/v1/api/productos?limit=20&offset="
     
   }
@@ -17,30 +19,18 @@ export class ProductosService {
     
     return this.http.post(`${this.baseUrl}${100}`,data);
 }
+
+
+getProducts(): Observable<any> {
+  return this.firestore.collection('Productos').snapshotChanges()
 }
-import { Injectable } from '@angular/core';
-import { Producto } from '../models/producto';
 
-import { AngularFirestore } from '@angular/fire/compat/firestore'
-import { Observable } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ProductosService {
-
-  constructor(private firestore: AngularFirestore) { 
-
-  }
-
-  getProducts(): Observable<any> {
-    return this.firestore.collection('Productos').snapshotChanges()
-  }
-
-  createProduct(producto: Producto): Promise<any>{
-    return this.firestore.collection('Productos').add(producto)
-    
-  }
-
+createProduct(producto: Producto): Promise<any>{
+  return this.firestore.collection('Productos').add(producto)
+  
+}
 
 }
+
+
+
