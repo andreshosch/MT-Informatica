7bbee743b45959a3995a3c4f6c8f9b6fff9955c0
Andrés Hosch <andreshosch@hotmail.com>
2023-10-30 23:00:54 -0300

    fix envio formulario contacto

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+ formData:any={}
+ private formUrl='https://formspree.io/f/xrgvgvvq'
 
+  constructor(private http: HttpClient) {
+
+  }
+  submitForm() {
+    this.http.post(this.formUrl, this.formData).subscribe(
+      (response) => {
+        console.log('Formulario enviado con éxito', response);
+      },
+      (error) => {
+        console.error('Error al enviar el formulario', error);
+      }
+    )
+  }
 }
+
